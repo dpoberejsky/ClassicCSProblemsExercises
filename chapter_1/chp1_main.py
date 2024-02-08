6597a529247478ce4037528ead61cd8cef242964
David Poberejsky <davidpob@Davids-MBP.attlocal.net>
2024-02-07 22:45:46 -0600

    Initial fib_recur function

---
+++ chapter_1/chp1_main.py
@@ -0,0 +1,12 @@
+from imports import *
+
+def fib_recur(n: int) -> int:
+    #Simple recursive function for finding fib numbers
+    if n <= 2:
+        return n
+    return fib_recur(n-1) + fib_recur(n-2)
+
+
+
+
+
