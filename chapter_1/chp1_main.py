d3deded5649183ff03b4a5157c81fa46d260408d
David Poberejsky <davidpob@Davids-MBP.attlocal.net>
2024-05-15 21:52:47 -0500

    Old code

--- chapter_1/chp1_main.py
+++ chapter_1/chp1_main.py
@@ -7,6 +7,27 @@ def fib_recur(n: int) -> int:
     return fib_recur(n-1) + fib_recur(n-2)
 
 
-#write function to get minimum value in list
 
+memo = {0: 0, 1:1}
+def fib_memo(n: int) -> int:
+    #Recursive function using memoization to speed up processing
+    if n not in memo.keys():
+        memo[n] = fib_memo(n-2) + fib_memo(n-1)
+    return memo[n]
 
+#Python auto caching
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def fib_memo_auto(n: int) -> int:
+    if n < 2:
+        return n
+    return fib_memo_auto(n-2) + fib_memo_auto(n-1)
+
+#Tuple unpacking
+def fib_tuple(n: int) -> int:
+    if n == 0: return n
+    last = 0
+    next = 1
+    for _ in range(1, n):
+        last, next = next, last+next
+    return next
